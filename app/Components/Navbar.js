9edb14bffa81ec19c574e5f42216b3191d95faa5
Akshit Kala <akshitkala72@gmail.com>
2026-02-25 01:58:19 +0530

    build: setup Panelify CMS refactor

--- app/Components/Navbar.js
+++ app/Components/Navbar.js
@@ -1,116 +1,65 @@
 "use client";
+
+import content from "../../content.json";
+const {
+  navbar
+} = content;
 import React, { useState } from "react";
 import { useSession, signIn, signOut } from "next-auth/react";
 import Link from "next/link";
-
 const Navbar = () => {
-  const { data: session } = useSession();
+  const {
+    data: session
+  } = useSession();
   const [dropdown, setDropdown] = useState(false);
-
-  return (
-    <nav
-      className={`bg-gray-900 px-8 text-white transition-all duration-500 ease-in-out ${
-        dropdown ? "py-6" : "py-3"
-      } `}
-    >
+  return <nav className={`bg-gray-900 px-8 text-white transition-all duration-500 ease-in-out ${dropdown ? "py-6" : "py-3"} `}>
       <div className="flex justify-between items-center">
       <Link href={'/'}>
-      <div className="logo font-bold text-2xl">GetMeAChai!</div></Link> 
+      <div className="logo font-bold text-2xl">{navbar.navbar_logo}</div></Link> 
         <div className="butons flex justify-between gap-4 relative">
-          {session && (
-            <div className="dropdown">
-              <button
-                onClick={() => setDropdown(!dropdown)} onBlur={()=>{setDropdown(!dropdown)}}
-                id="dropdownDefaultButton"
-                className="text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2.5 inline-flex items-center dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800"
-                type="button"
-              >
+          {session && <div className="dropdown">
+              <button onClick={() => setDropdown(!dropdown)} onBlur={() => {
+            setDropdown(!dropdown);
+          }} id="dropdownDefaultButton" className="text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2.5 inline-flex items-center dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800" type="button">
                 welcome {session.user.name}
-                <svg
-                  className="w-2.5 h-2.5 ml-2"
-                  aria-hidden="true"
-                  xmlns="http://www.w3.org/2000/svg"
-                  fill="none"
-                  viewBox="0 0 10 6"
-                >
-                  <path
-                    stroke="currentColor"
-                    strokeLinecap="round"
-                    strokeLinejoin="round"
-                    strokeWidth="2"
-                    d="m1 1 4 4 4-4"
-                  />
+                <svg className="w-2.5 h-2.5 ml-2" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 10 6">
+                  <path stroke="currentColor" strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="m1 1 4 4 4-4" />
                 </svg>
               </button>
 
               {/* Animated Dropdown Menu */}
-              <div
-                id="dropdown"
-                className={`transition-all duration-300 ease-in-out overflow-hidden absolute mt-2 w-44 bg-white divide-y divide-gray-100 rounded-lg shadow dark:bg-gray-700 ${
-                  dropdown ? "max-h-60 opacity-100" : "max-h-0 opacity-0"
-                }`}
-              >
+              <div id="dropdown" className={`transition-all duration-300 ease-in-out overflow-hidden absolute mt-2 w-44 bg-white divide-y divide-gray-100 rounded-lg shadow dark:bg-gray-700 ${dropdown ? "max-h-60 opacity-100" : "max-h-0 opacity-0"}`}>
                 <ul className="py-2 text-sm text-gray-700 dark:text-gray-200">
                   <li>
-                    <Link
-                      href="/dashboard"
-                      className="block px-4 py-2 hover:bg-gray-100 dark:hover:bg-gray-600 dark:hover:text-white"
-                    >
+                    <Link href="/dashboard" className="block px-4 py-2 hover:bg-gray-100 dark:hover:bg-gray-600 dark:hover:text-white">
                       Dashboard
                     </Link>
                   </li>
                   <li>
-                    <Link
-                      href="#"
-                      className="block px-4 py-2 hover:bg-gray-100 dark:hover:bg-gray-600 dark:hover:text-white"
-                    >
+                    <Link href="#" className="block px-4 py-2 hover:bg-gray-100 dark:hover:bg-gray-600 dark:hover:text-white">
                       Settings
                     </Link>
                   </li>
                   <li>
-                    <Link
-                      href="#"
-                      className="block px-4 py-2 hover:bg-gray-100 dark:hover:bg-gray-600 dark:hover:text-white"
-                    >
+                    <Link href="#" className="block px-4 py-2 hover:bg-gray-100 dark:hover:bg-gray-600 dark:hover:text-white">
                       Earnings
                     </Link>
                   </li>
                   <li>
-                    <button
-                      onClick={() => signOut()}
-                      className="w-full text-left px-4 py-2 hover:bg-gray-100 dark:hover:bg-gray-600 dark:hover:text-white"
-                    >
+                    <button onClick={() => signOut()} className="w-full text-left px-4 py-2 hover:bg-gray-100 dark:hover:bg-gray-600 dark:hover:text-white">
                       Sign out
                     </button>
                   </li>
                 </ul>
               </div>
-            </div>
-          )}
-          {!session && (
-            <Link href={"/login"}>
-              <button
-                type="button"
-                className="text-white bg-gradient-to-br from-purple-600 to-blue-500 hover:bg-gradient-to-bl focus:ring-4 focus:outline-none focus:ring-blue-300 dark:focus:ring-blue-800 font-medium rounded-lg text-sm px-5 py-2.5 text-center"
-              >
-                Log in
-              </button>
-            </Link>
-          )}
+            </div>}
+          {!session && <Link href={"/login"}>
+              <button type="button" className="text-white bg-gradient-to-br from-purple-600 to-blue-500 hover:bg-gradient-to-bl focus:ring-4 focus:outline-none focus:ring-blue-300 dark:focus:ring-blue-800 font-medium rounded-lg text-sm px-5 py-2.5 text-center">{navbar.navbar_login}</button>
+            </Link>}
 
-          {session && (
-            <button
-              onClick={() => signOut()}
-              type="button"
-              className="text-white bg-gradient-to-br from-purple-600 to-blue-500 hover:bg-gradient-to-bl focus:ring-4 focus:outline-none focus:ring-blue-300 dark:focus:ring-blue-800 font-medium rounded-lg text-sm px-5 py-2.5 text-center"
-            >
-              Log out
-            </button>
-          )}
+          {session && <button onClick={() => signOut()} type="button" className="text-white bg-gradient-to-br from-purple-600 to-blue-500 hover:bg-gradient-to-bl focus:ring-4 focus:outline-none focus:ring-blue-300 dark:focus:ring-blue-800 font-medium rounded-lg text-sm px-5 py-2.5 text-center">{navbar.navbar_logout}</button>}
         </div>
       </div>
-    </nav>
-  );
+    </nav>;
 };
-
 export default Navbar;
--- app/[username]/page.js
+++ app/[username]/page.js
@@ -1,42 +1,43 @@
+import content from "../../content.json";
+const {
+  page
+} = content;
 import React from "react";
 import { useSession, signIn, signOut } from "next-auth/react";
 import Link from "next/link";
-const Page = async ({ params }) => {
-  return (
-    <>
+const Page = async ({
+  params
+}) => {
+  return <>
       <div className="relative ">
-        <img className="object-cover w-full" src="/sideprofile.jpeg" alt="" />
-        <img
-          className="w-50 relative bottom-[110px] mx-auto"
-          src="/profile.png"
-          alt=""
-        />
+        <img className="object-cover w-full" src={page.page_image_1} alt="" />
+        <img className="w-50 relative bottom-[110px] mx-auto" src={page.page_image_2} alt="" />
         <div className="w-[80%] gap-2 flex mx-auto">
           <div className="w-1/2 p-5 my-10 bg-slate-900/80 text-white min-h-[100px] rounded-xl shadow-lg">
-            <h2 className="text-3xl font-bold mb-4">Supporters</h2>
+            <h2 className="text-3xl font-bold mb-4">{page.page_supporters}</h2>
 
             <ul className="flex flex-col gap-y-2 min-h-[200px]  pr-2">
-              <li className="bg-slate-800 px-4 py-2 rounded-md hover:bg-slate-700 transition duration-200 ease-in-out flex gap-5 items-center"><img width={40} className="rounded-[50%]" src="/avtaar.gif" alt="" />
+              <li className="bg-slate-800 px-4 py-2 rounded-md hover:bg-slate-700 transition duration-200 ease-in-out flex gap-5 items-center"><img width={40} className="rounded-[50%]" src={page.page_image_3} alt="" />
 
                 akshit paid u 300 rupeee
               </li>
-              <li className="bg-slate-800 px-4 py-2 rounded-md hover:bg-slate-700 transition duration-200 ease-in-out flex gap-5 items-center"><img width={40} className="rounded-[50%]" src="/avtaar.gif" alt="" />
+              <li className="bg-slate-800 px-4 py-2 rounded-md hover:bg-slate-700 transition duration-200 ease-in-out flex gap-5 items-center"><img width={40} className="rounded-[50%]" src={page.page_image_3} alt="" />
 
                 akshit paid u 300 rupeee
               </li>
-              <li className="bg-slate-800 px-4 py-2 rounded-md hover:bg-slate-700 transition duration-200 ease-in-out flex gap-5 items-center"><img width={40} className="rounded-[50%]" src="/avtaar.gif" alt="" />
+              <li className="bg-slate-800 px-4 py-2 rounded-md hover:bg-slate-700 transition duration-200 ease-in-out flex gap-5 items-center"><img width={40} className="rounded-[50%]" src={page.page_image_3} alt="" />
 
                 akshit paid u 300 rupeee
               </li>
-              <li className="bg-slate-800 px-4 py-2 rounded-md hover:bg-slate-700 transition duration-200 ease-in-out flex gap-5 items-center"><img width={40} className="rounded-[50%]" src="/avtaar.gif" alt="" />
+              <li className="bg-slate-800 px-4 py-2 rounded-md hover:bg-slate-700 transition duration-200 ease-in-out flex gap-5 items-center"><img width={40} className="rounded-[50%]" src={page.page_image_3} alt="" />
 
                 akshit paid u 300 rupeee
               </li>
-              <li className="bg-slate-800 px-4 py-2 rounded-md hover:bg-slate-700 transition duration-200 ease-in-out flex gap-5 items-center"><img width={40} className="rounded-[50%]" src="/avtaar.gif" alt="" />
+              <li className="bg-slate-800 px-4 py-2 rounded-md hover:bg-slate-700 transition duration-200 ease-in-out flex gap-5 items-center"><img width={40} className="rounded-[50%]" src={page.page_image_3} alt="" />
 
                 akshit paid u 300 rupeee
               </li>
-              <li className="bg-slate-800 px-4 py-2 rounded-md hover:bg-slate-700 transition duration-200 ease-in-out flex gap-5 items-center"><img width={40} className="rounded-[50%]" src="/avtaar.gif" alt="" />
+              <li className="bg-slate-800 px-4 py-2 rounded-md hover:bg-slate-700 transition duration-200 ease-in-out flex gap-5 items-center"><img width={40} className="rounded-[50%]" src={page.page_image_3} alt="" />
 
                 akshit paid u 300 rupeee
               </li>
@@ -45,30 +46,14 @@ const Page = async ({ params }) => {
           </div>
 
           <div className="w-1/2 p-5 my-10 bg-slate-900/80 text-white min-h-[100px] rounded-xl shadow-lg">
-            <h2 className="text-3xl font-bold mb-4 text-center">
-              Make a Payment
-            </h2>
+            <h2 className="text-3xl font-bold mb-4 text-center">{page.page_make_a_payment}</h2>
 
             <div className="inputs flex flex-col gap-4 w-[90%] mx-auto">
-              <input
-                type="text"
-                className="w-full bg-slate-800 text-white placeholder-gray-400 border border-slate-600 rounded-lg px-4 py-2 focus:outline-none focus:ring-2 focus:ring-blue-500 transition duration-300 ease-in-out"
-                placeholder="Enter Name"
-              />
-              <input
-                type="text"
-                className="w-full bg-slate-800 text-white placeholder-gray-400 border border-slate-600 rounded-lg px-4 py-2 focus:outline-none focus:ring-2 focus:ring-blue-500 transition duration-300 ease-in-out"
-                placeholder="Enter Message"
-              />
-              <input
-                type="number"
-                className="w-full bg-slate-800 text-white placeholder-gray-400 border border-slate-600 rounded-lg px-4 py-2 focus:outline-none focus:ring-2 focus:ring-blue-500 transition duration-300 ease-in-out"
-                placeholder="Enter Amount"
-              />
+              <input type="text" className="w-full bg-slate-800 text-white placeholder-gray-400 border border-slate-600 rounded-lg px-4 py-2 focus:outline-none focus:ring-2 focus:ring-blue-500 transition duration-300 ease-in-out" placeholder="Enter Name" />
+              <input type="text" className="w-full bg-slate-800 text-white placeholder-gray-400 border border-slate-600 rounded-lg px-4 py-2 focus:outline-none focus:ring-2 focus:ring-blue-500 transition duration-300 ease-in-out" placeholder="Enter Message" />
+              <input type="number" className="w-full bg-slate-800 text-white placeholder-gray-400 border border-slate-600 rounded-lg px-4 py-2 focus:outline-none focus:ring-2 focus:ring-blue-500 transition duration-300 ease-in-out" placeholder="Enter Amount" />
 
-              <button className="mt-2 bg-blue-600 hover:bg-blue-700 text-white py-2 rounded-lg transition duration-300 ease-in-out">
-                Pay
-              </button>
+              <button className="mt-2 bg-blue-600 hover:bg-blue-700 text-white py-2 rounded-lg transition duration-300 ease-in-out">{page.page_pay}</button>
 
               <div className="pay flex justify-center gap-4 mt-4">
                 <button className="bg-slate-700 text-white px-4 py-2 rounded-lg hover:bg-slate-600 hover:scale-105 transition duration-300 ease-in-out">
@@ -85,8 +70,6 @@ const Page = async ({ params }) => {
           </div>
         </div>
       </div>
-    </>
-  );
+    </>;
 };
-
 export default Page;
